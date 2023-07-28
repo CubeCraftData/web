ed705dd92b727037811530c396bf076db4eacc3b
MrSterdy <mrsterdy@gmail.com>
2023-07-28 18:07:53 +0400

    Make header responsive

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,15 +3,33 @@ import { Link } from "react-router-dom";
 import "./index.css";
 
 export default function Index() {
+    function show() {
+        document.querySelector(".header-items")!.classList.toggle("hidden");
+    }
+
     return (
         <header>
             <nav>
-                <ul>
+                <ul className="header-logo">
                     <li>
-                        <Link to="/" className="logo-url"><strong>CubeCraft Data</strong></Link>
+                        <Link to="/" className="logo-url">
+                            <strong>CubeCraft Data</strong>
+                        </Link>
+
+                        <button onClick={show}>
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                id="Outline"
+                                viewBox="0 0 24 24"
+                            >
+                                <rect y="11" width="24" height="2" rx="1" />
+                                <rect y="4" width="24" height="2" rx="1" />
+                                <rect y="18" width="24" height="2" rx="1" />
+                            </svg>
+                        </button>
                     </li>
                 </ul>
-                <ul>
+                <ul className="header-items hidden">
                     <li>
                         <Link to="/leaderboards">Leaderboards</Link>
                     </li>
