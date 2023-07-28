42ba73b8737a1a6c22cd68823c65375a7f52d7a9
MrSterdy <mrsterdy@gmail.com>
2023-07-28 18:40:37 +0400

    Fix eventsource not being bundled correctly

--- vite.config.ts
+++ vite.config.ts
@@ -3,17 +3,28 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 import { NodeModulesPolyfillPlugin } from "@esbuild-plugins/node-modules-polyfill";
+import rollupNodePolyfill from "rollup-plugin-node-polyfills";
+import inject from "@rollup/plugin-inject";
 
 export default defineConfig({
     plugins: [react()],
     resolve: {
         alias: {
             util: "rollup-plugin-node-polyfills/polyfills/util",
+            events: "rollup-plugin-node-polyfills/polyfills/events",
         },
     },
     optimizeDeps: {
         esbuildOptions: {
             plugins: [NodeModulesPolyfillPlugin()],
+        },
+    },
+    build: {
+        rollupOptions: {
+            plugins: [
+                inject({ Buffer: ["Buffer", "Buffer"] }),
+                rollupNodePolyfill(),
+            ],
         },
     },
 });
