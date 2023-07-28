55029ff157c33b7d27a2e8a756659d198563c441
MrSterdy <mrsterdy@gmail.com>
2023-07-28 07:03:06 +0400

    Install pico css

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import Root from "./routes";
-
 import {
     createBrowserRouter,
     createRoutesFromElements,
     Route,
     RouterProvider,
 } from "react-router-dom";
+
+import "@picocss/pico";
+
+import Root from "./routes";
 
 const router = createBrowserRouter(
     createRoutesFromElements(<Route path="/" element={<Root />}></Route>),
